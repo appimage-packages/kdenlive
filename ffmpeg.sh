ad828940df680bba64674a7ba38e201a5ca5d117
Scarlett Clark <sgclark@kde.org>
2017-03-05 08:37:21 -0700

    Adjust configure options for both ffmpeg and mlt

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -4,21 +4,29 @@ tar xjvf ffmpeg-snapshot.tar.bz2
 cd ffmpeg
  ./configure \
   --prefix="/opt/usr" \
-  --pkg-config-flags="--static" \
-  --extra-cflags="-I/opt/usr/include" \
-  --extra-ldflags="-L/opt/usr/lib" \
-  --bindir="/opt/usr/bin" \
-  --enable-gpl \
-  --enable-libass \
-  --enable-libfreetype \
+  --progs-suffix=mlt \
+  --build-suffix=mlt \
+  --enable-shared \
   --enable-libmp3lame \
-  --enable-libopus \
-  --enable-libtheora \
+  --enable-gpl \
+  --enable-libfaac \
   --enable-libvorbis \
-  --enable-libvpx \
+  --enable-pthreads \
+  --enable-libfaac \
+  --enable-libxvid \
+  --enable-x11grab \
+  --enable-libgsm \
   --enable-libx264 \
-  --enable-libx265 \
-  --enable-nonfree
-make
+  --enable-libtheora \
+  --enable-libdc1394 \
+  --enable-nonfree \
+  --disable-stripping \
+  --enable-avfilter \
+  --enable-libschroedinger \
+  --enable-libopencore-amrnb \
+  --enable-libopencore-amrwb \
+  --enable-version3 \
+  --enable-libvpx
+make -j4
 make install
 hash -r
