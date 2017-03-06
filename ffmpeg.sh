1d52d65281dbbdde642b2b8bdaf4a793e2593a89
Scarlett Clark <sgclark@kde.org>
2017-03-06 08:38:38 -0700

    Add error handling to ffmpeg build

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -x
 wget http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
 tar xjvf ffmpeg-snapshot.tar.bz2
-cd ffmpeg
+
+if cd ffmpeg \
  ./configure \
   --prefix="/opt/usr" \
   --progs-suffix=mlt \
@@ -12,7 +14,6 @@ cd ffmpeg
   --enable-libfaac \
   --enable-libvorbis \
   --enable-pthreads \
-  --enable-libfaac \
   --enable-libxvid \
   --enable-x11grab \
   --enable-libgsm \
@@ -26,7 +27,15 @@ cd ffmpeg
   --enable-libopencore-amrnb \
   --enable-libopencore-amrwb \
   --enable-version3 \
-  --enable-libvpx
-make -j4
-make install
-hash -r
+  --enable-libvpx; then
+	make -j4
+  make install
+else
+	error_exit "$LINENO: An error has occurred.. Aborting."
+fi
+
+function error_exit
+{
+	echo "$1" 1>&2
+	exit 1
+}
