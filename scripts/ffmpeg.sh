cb3b8160dd68278f631862cd5e86a5e68147cf70
Scarlett Clark <sgclark@kde.org>
2017-03-16 15:52:32 -0700

    Add enable-frei0r

--- scripts/ffmpeg.sh
+++ scripts/ffmpeg.sh
@@ -17,7 +17,7 @@ fi
 if cd ffmpeg; then
 	 ./configure --prefix="/opt/usr" --extra-ldflags="-L/opt/usr//lib -lstdc++ -lm -lrt -ldl" --extra-cflags="-I/opt/usr/include" --pkg-config-flags="--static" --progs-suffix=mlt --build-suffix=mlt --enable-shared --enable-libmp3lame --enable-gpl --enable-libvorbis --enable-pthreads --enable-libxvid --enable-x11grab --enable-libgsm \
  	 --enable-libx264 --enable-libtheora --enable-libdc1394 --enable-nonfree --disable-stripping --enable-avfilter --enable-libschroedinger --enable-libopencore-amrnb --enable-libopencore-amrwb \
- 	 --enable-version3 --enable-libvpx && \
+ 	 --enable-version3 --enable-libvpx --enable-frei0r && \
 	  make -j4 && make install
 else
 	error_exit "$LINENO: An error has occurred.. Aborting."
