a969dbea4511e8fbb486fcfdf4aded424c125c88
Scarlett Clark <sgclark@kde.org>
2017-03-16 15:03:08 -0700

    Bring in original ffmpeg configure opts

--- scripts/ffmpeg.sh
+++ scripts/ffmpeg.sh
@@ -7,7 +7,7 @@ function error_exit
 	echo "$1" 1>&2
 	exit 1
 }
-mv /opt/usr/./include/frei0r.h /opt/usr/include
+mv /opt/usr/./include/frei0r.h /opt/usr/include/
 
 if wget http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2; then
 	tar xjvf ffmpeg-snapshot.tar.bz2
@@ -16,8 +16,9 @@ else
 fi
 
 if cd ffmpeg; then
-	 ./configure --prefix="/opt/usr" --extra-ldflags="-L/opt/usr//lib -lstdc++ -lm -lrt -ldl" --extra-cflags="-I/opt/usr/include" --pkg-config-flags="--static" --enable-shared --enable-avfilter \
-	 --enable-gpl --enable-avresample --enable-avformat --enable-frei0r --enable-libx265 --enable-libvpx --enable-libx264 --disable-doc && \
+	 ./configure --prefix="/opt/usr" --extra-ldflags="-L/opt/usr//lib -lstdc++ -lm -lrt -ldl" --extra-cflags="-I/opt/usr/include" --pkg-config-flags="--static" --progs-suffix=mlt --build-suffix=mlt --enable-shared --enable-libmp3lame --enable-gpl --enable-libvorbis --enable-pthreads --enable-libxvid --enable-x11grab --enable-libgsm \
+ 	 --enable-libx264 --enable-libtheora --enable-libdc1394 --enable-nonfree --disable-stripping --enable-avfilter --enable-libschroedinger --enable-libopencore-amrnb --enable-libopencore-amrwb \
+ 	 --enable-version3 --enable-libvpx && \
 	  make -j4 && make install
 else
 	error_exit "$LINENO: An error has occurred.. Aborting."
