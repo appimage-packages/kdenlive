20a100d623d2a452682d8027ea2e7b8574974876
Scarlett Clark <sgclark@kde.org>
2017-03-16 16:08:02 -0700

    remove enable-x11grab

--- scripts/ffmpeg.sh
+++ scripts/ffmpeg.sh
@@ -15,7 +15,8 @@ else
 fi
 
 if cd ffmpeg; then
-	 ./configure --prefix="/opt/usr" --extra-ldflags="-L/opt/usr//lib -lstdc++ -lm -lrt -ldl" --extra-cflags="-I/opt/usr/include" --pkg-config-flags="--static" --progs-suffix=mlt --build-suffix=mlt --enable-shared --enable-libmp3lame --enable-gpl --enable-libvorbis --enable-pthreads --enable-libxvid --enable-x11grab --enable-libgsm \
+	 ./configure --prefix="/opt/usr" --extra-ldflags="-L/opt/usr//lib -lstdc++ -lm -lrt -ldl" --extra-cflags="-I/opt/usr/include" --pkg-config-flags="--static" --progs-suffix=mlt --build-suffix=mlt --enable-shared \
+	 --enable-libmp3lame --enable-gpl --enable-libvorbis --enable-pthreads --enable-libxvid --enable-libgsm \
  	 --enable-libx264 --enable-libtheora --enable-libdc1394 --enable-nonfree --disable-stripping --enable-avfilter --enable-libschroedinger --enable-libopencore-amrnb --enable-libopencore-amrwb \
  	 --enable-version3 --enable-libvpx --enable-frei0r && \
 	  make -j4 && make install
