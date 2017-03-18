a6a1572238931f84976c7209dc2567cc180dd933
Scarlett Clark <sgclark@kde.org>
2017-03-18 14:32:21 -0700

    debug ls

--- scripts/ffmpeg.sh
+++ scripts/ffmpeg.sh
@@ -14,6 +14,7 @@ export CPLUS_INCLUDE_PATH=/opt/usr/include:/usr/include:$CPLUS_INCLUDE_PATH
 if cd ffmpeg; then
 	 ./configure --prefix="/usr" --disable-doc --disable-ffserver --enable-gpl --enable-version3 --enable-shared --enable-static --enable-debug --enable-pthreads --enable-runtime-cpudetect --enable-frei0r --enable-avfilter && \
 	  make -j4 && make install
+		ls /usr/lib/
 else
 	error_exit "$LINENO: An error has occurred.. Aborting."
 fi
