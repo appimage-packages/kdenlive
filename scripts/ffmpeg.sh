ca0ac7d1214df52bec32853a337a044499cea2b5
Scarlett Clark <sgclark@kde.org>
2017-03-18 16:38:45 -0700

    ffmpeg in opt/usr and only static

--- scripts/ffmpeg.sh
+++ scripts/ffmpeg.sh
@@ -12,7 +12,7 @@ git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg
 
 export CPLUS_INCLUDE_PATH=/opt/usr/include:/usr/include:$CPLUS_INCLUDE_PATH
 if cd ffmpeg; then
-	 ./configure --prefix="/usr" --disable-doc --disable-ffserver --enable-gpl --enable-version3 --enable-shared --enable-static --enable-debug --enable-pthreads --enable-runtime-cpudetect --enable-frei0r --enable-avfilter && \
+	 ./configure --prefix="/opt/usr" --disable-doc --disable-ffserver --enable-gpl --enable-version3 --disable-shared --enable-static --enable-debug --enable-pthreads --enable-runtime-cpudetect --enable-frei0r --enable-avfilter && \
 	  make -j4 && make install
 		ls /usr/lib/
 else
