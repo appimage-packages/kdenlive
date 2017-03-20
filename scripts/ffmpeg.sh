50778b6f586a8886d2441c247cee63b99fc9011b
Scarlett Clark <sgclark@kde.org>
2017-03-20 12:49:46 -0700

    enable the missing codecs in ffmpeg build

--- scripts/ffmpeg.sh
+++ scripts/ffmpeg.sh
@@ -12,7 +12,8 @@ git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg
 
 export CPLUS_INCLUDE_PATH=/opt/usr/include:/usr/include:$CPLUS_INCLUDE_PATH
 if cd ffmpeg; then
-	 ./configure --prefix="/opt/usr" --disable-doc --disable-ffserver --enable-gpl --enable-version3 --enable-shared --disable-static --enable-debug --enable-pthreads --enable-runtime-cpudetect --enable-frei0r --enable-avfilter && \
+	 ./configure --prefix="/opt/usr" --disable-doc --disable-ffserver --enable-gpl --enable-version3 --enable-shared --disable-static --enable-debug --enable-pthreads --enable-runtime-cpudetect --enable-frei0r --enable-avfilter \
+	 --enable-libvpx --enable-libx264 --enable-libvpx-vp9 --enable-libx265 && \
 	  make -j4 && make install
 		ls /opt/usr/include/
 else
