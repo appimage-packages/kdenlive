7b4cf5c59e7e00b12b5df42cc36bcaf78a1c661b
Scarlett Clark <sgclark@kde.org>
2017-03-21 10:40:27 -0700

    add -fPIC wanted by ffmpeg

--- scripts/ffmpeg.sh
+++ scripts/ffmpeg.sh
@@ -6,7 +6,7 @@ export PKG_CONFIG="pkg-config --static"
 export CPATH=/opt/usr/include:/opt/usr/include/corvusoft:/opt/usr/include/boost/system:/opt/usr/include/boost:/usr/include
 export LDFLAGS="-L/opt/usr/library -L/opt/usr/lib $LDFLAGS"
 export CFLAGS="-I/opt/usr/include $CFLAGS"
-
+export CXXFLAGS="-std=c++11 -fPIC"
 function error_exit
 {
 	echo "$1" 1>&2
@@ -17,7 +17,7 @@ git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg
 
 
 if cd ffmpeg; then
-	 ./configure --prefix="/opt/usr" --disable-doc --disable-ffserver --enable-gpl --enable-version3 --enable-shared --disable-static --enable-debug --enable-pthreads --enable-runtime-cpudetect --enable-frei0r --enable-avfilter \
+	 ./configure --prefix="/opt/usr" --disable-doc --disable-ffserver --enable-gpl --enable-version3 --enable-shared --enable-static --enable-debug --enable-pthreads --enable-runtime-cpudetect --enable-frei0r --enable-avfilter \
 	 --enable-libvpx --enable-libx264 --enable-libx265 && \
 	  make -j4 && make install
 		ls /opt/usr/include/
