204cefa3b18f452c579b97ae59a7ab9c33419b61
Scarlett Clark <sgclark@kde.org>
2017-03-20 18:47:10 -0700

    set some env vars for ffmpeg

--- scripts/ffmpeg.sh
+++ scripts/ffmpeg.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 set -x
-
+export LD_LIBRARY_PATH=/opt/usr/lib:/opt/usr/library:/opt/usr/lib/x86_64-linux-gnu:/usr/lib:/usr/lib64
+export PKG_CONFIG="pkg-config --static"
+export CPATH=/opt/usr/include:/opt/usr/include/corvusoft:/opt/usr/include/boost/system:/opt/usr/include/boost:/usr/include
+export LDFLAGS="-L/opt/usr/library -L/opt/usr/lib $LDFLAGS"
+export CFLAGS="-I/opt/usr/include $CFLAGS"
 function error_exit
 {
 	echo "$1" 1>&2
--- scripts/mlt.sh
+++ scripts/mlt.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export LD_LIBRARY_PATH=/opt/usr/lib:/opt/usr/library:/opt/usr/lib/x86_64-linux-gnu:/usr/lib:/usr/lib64
+export PKG_CONFIG="pkg-config --static"
+export CPATH=/opt/usr/include:/opt/usr/include/corvusoft:/opt/usr/include/boost/system:/opt/usr/include/boost:/usr/include
+export LDFLAGS="-L/opt/usr/library -L/opt/usr/lib $LDFLAGS"
 export CFLAGS="-I/opt/usr/include $CFLAGS"
 
 set -e
