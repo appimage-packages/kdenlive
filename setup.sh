29acf5c0f275d532adb7f59ee617f12c153ecf01
Scarlett Clark <sgclark@kde.org>
2017-06-14 05:15:03 -0700

    Get gcc 5 from ppa.

--- setup.sh
+++ setup.sh
@@ -22,4 +22,10 @@ export WORKSPACE=`pwd`
 export CPATH=/opt/usr/include:/usr/include
 echo $WORKSPACE
 
+add-apt-repository ppa:ubuntu-toolchain-r/test
+apt-get update
+apt-get install gcc-5 g++-5
+
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
+
 cd /in/ && rspec appimage-template/spec/recipe_rspec.rb --fail-fast
